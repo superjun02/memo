b813c207f49d34da32346a063575165549537a15
DESKTOP-848094R\super <superjun02@naver.com>
2021-09-24 21:27:11 +0900

    초기 환경 세팅

---
+++ src/main/java/com/memo/test/TestController.java
@@ -0,0 +1,15 @@
+package com.memo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping("/test1")
+	@ResponseBody
+	public String test1() {
+		return "Hello world!!!";
+	}
+}
